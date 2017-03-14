5e27bf32b17227400a356b5fad644540047635f3
zhangsan <1399846221@qq.com>
2017-03-14 17:13:35 +0800

    add

--- app/src/main/java/com/example/administrator/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/example/administrator/broadcastbestpractice/LoginActivity.java
@@ -1,18 +1,24 @@
 package com.example.administrator.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
 
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+
+    private SharedPreferences.Editor editor;
+    private SharedPreferences pre;
+    private CheckBox rememberpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,18 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
+
+        pre = PreferenceManager.getDefaultSharedPreferences(this);
+        rememberpass = (CheckBox) findViewById(R.id.remember_pass);
+        boolean isRemember = pre.getBoolean("remember_password",false);
+        if (isRemember){
+            String account = pre.getString("account","");
+            String password = pre.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberpass.setChecked(true);
+        }
+
         login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +45,17 @@ public class LoginActivity extends AppCompatActivity {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
                 if (account.equals("admin") && password.equals("123456")){
+
+                    editor = pre.edit();
+                    if (rememberpass.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else {
+                        editor.clear();
+                    }
+                    editor.apply();
+
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
